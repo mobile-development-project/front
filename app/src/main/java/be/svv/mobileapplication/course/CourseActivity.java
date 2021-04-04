04a6da60cc294c8f9c839c08c05dcaa630867bb3
Van Vreckem <svanvreckem@helb-prigogine.be>
2021-04-04 10:47:52 +0200

    refactor: return ArrayList<Course> instead of a simple array

--- app/src/main/java/be/svv/mobileapplication/course/CourseActivity.java
+++ app/src/main/java/be/svv/mobileapplication/course/CourseActivity.java
@@ -1,29 +1,22 @@
-package be.svv.mobileapplication;
+package be.svv.mobileapplication.course;
 
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.ListAdapter;
-import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import be.svv.adapter.CourseAdapter;
 import be.svv.entity.Course;
-import be.svv.entity.Entity;
-import be.svv.globals.ApiUrl;
+import be.svv.mobileapplication.R;
 import be.svv.repository.CourseRepository;
-import be.svv.service.Volley.VolleyCallback;
 
 public class CourseActivity extends AppCompatActivity
 {
@@ -40,7 +33,7 @@ public class CourseActivity extends AppCompatActivity
         {
             GsonBuilder gsonBuilder = new GsonBuilder();
             Gson gson = gsonBuilder.create();
-            Course[] courses = gson.fromJson(result, Course[].class);
+            ArrayList<Course> courses = gson.fromJson(result, new TypeToken<List<Course>>(){}.getType());
             CourseAdapter adapter = new CourseAdapter(CourseActivity.this, courses);
             recycler_view.setAdapter(adapter);
             recycler_view.setLayoutManager(new LinearLayoutManager(CourseActivity.this));
