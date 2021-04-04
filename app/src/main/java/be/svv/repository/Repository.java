14fca613cde6aa435ed03c6117be0eb875c82573
Van Vreckem <svanvreckem@helb-prigogine.be>
2021-04-04 10:51:05 +0200

    refactor & docs: removed useless import and added some javadoc

--- app/src/main/java/be/svv/repository/Repository.java
+++ app/src/main/java/be/svv/repository/Repository.java
@@ -5,11 +5,8 @@ import android.util.Log;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 
-import be.svv.globals.ApiUrl;
 import be.svv.service.Volley.VolleyCallback;
 import be.svv.service.Volley.VolleySingleton;
 
@@ -27,16 +24,34 @@ public class Repository
         queue = VolleySingleton.getInstance(context).getRequestQueue();
     }
 
+    /**
+     * Returns one item
+     *
+     * @param id
+     * @param callback
+     */
     public void findById (int id, final VolleyCallback callback)
     {
         stringRequest(Request.Method.GET, URL + "/" + id, callback);
     }
 
+    /**
+     * Returns a collection of items
+     *
+     * @param callback
+     */
     public void findAll (final VolleyCallback callback)
     {
         stringRequest(Request.Method.GET, URL, callback);
     }
 
+    /**
+     * Fetches the data and call the callback passed in argument
+     *
+     * @param method
+     * @param url
+     * @param callback
+     */
     private void stringRequest (int method, String url, final VolleyCallback callback)
     {
         StringRequest stringRequest = new StringRequest(method, url, response ->
