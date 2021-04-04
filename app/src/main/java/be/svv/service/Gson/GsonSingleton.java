e4f38934e8e13a5ca3fedabfd6dce50c72afa12f
Van Vreckem <svanvreckem@helb-prigogine.be>
2021-04-04 12:31:42 +0200

    feat: Gson singleton

---
+++ app/src/main/java/be/svv/service/Gson/GsonSingleton.java
@@ -0,0 +1,40 @@
+package be.svv.service.Gson;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.lang.reflect.Type;
+
+public class GsonSingleton
+{
+
+    private static GsonSingleton instance;
+    private GsonBuilder gsonBuilder;
+    private Gson gson;
+
+    private GsonSingleton ()
+    {
+        gsonBuilder = new GsonBuilder();
+        gson = gsonBuilder.create();
+    }
+
+    public static synchronized GsonSingleton getInstance ()
+    {
+        if (instance == null)
+        {
+            instance = new GsonSingleton();
+        }
+        return instance;
+    }
+
+    public <T> T fromJson (String json, Type type)
+    {
+        return gson.fromJson(json, type);
+    }
+
+    public String toJson (Object object)
+    {
+        return gson.toJson(object);
+    }
+
+}
