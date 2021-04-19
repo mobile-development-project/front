a5fd4df7e581e0da232d4fa3a170752533fdae49
Van Vreckem <svanvreckem@helb-prigogine.be>
2021-04-19 11:53:09 +0200

    feat: globals url

---
+++ app/src/main/java/be/svv/globals/Url.java
@@ -0,0 +1,12 @@
+package be.svv.globals;
+
+public class Url
+{
+
+    public static final String COURSES = "/api/courses/";
+    public static final String ASSIGNMENTS = "/api/assignments/";
+    public static final String CATEGORIES = "/api/categories/";
+    public static final String NOTES = "/api/notes/";
+    public static final String USERS = "/api/users/";
+
+}
